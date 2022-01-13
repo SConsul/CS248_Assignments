8e73fc6c20137bcfd84a1b4f6550bab42f1ab3d1
SConsul <sarthakconsul@gmail.com>
2022-01-12 18:25:01 -0800

    task 3 done, need to clear screen on updating viewpoint

--- draw-svg/src/viewport.cpp
+++ draw-svg/src/viewport.cpp
@@ -9,11 +9,15 @@ void ViewportImp::set_viewbox( float x, float y, float span ) {
   // Task 3 (part 2): 
   // Set svg to normalized device coordinate transformation. 
   // Your input arguments are defined as SVG canvans coordinates.
-
   this->x = x;
   this->y = y;
   this->span = span; 
 
+  double data[9] = {0.5/span, 0.0, 0.5-0.5*x/span, 0.0, 0.5/span, 0.5-0.5*y/span, 0.0, 0.0, 1.0};
+  Matrix3x3 m(data);
+  set_canvas_to_norm(m);
+
+  
 }
 
 void ViewportImp::update_viewbox( float dx, float dy, float scale ) { 
